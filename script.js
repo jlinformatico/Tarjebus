b89b5d9210c03ab2b9e89adc2aab1ec31b4feef7
felixricarb <felix.ricardo.barros@gmail.com>
2014-06-01 07:17:53 -0300

    Agregamos las direciones de resistencia

--- script.js
+++ script.js
@@ -1,16 +1,36 @@
 var map;
 
 function initialize() {
-
   var locations = [
-      ['Caja Municipal De Resistencia', 'Julio A. Roca 35, Resistencia, Chaco', -27.4540748, -58.981437, 2],
-      ['Tarjebus S.A', 'Av Italia 102, Resistencia, Chaco', -27.4517084, -58.987703, 1]
-    ];
+    // Resistencia
+    ['Polirubro Aye y Tati', 'Av Chaco 485', -27.4675118, -58.9763704, 1],
+    ['Maxikiosco Juan Lucas', 'Av Mac Lean 817', -27.4448822, -59.0082411, 2],
+    ['Mercadito Riki', 'Av Lavalle 1396', -27.4336536, -58.9918965, 3],
+    //['El Garage', 'Fray Bertaca 999. B. España', 0, 0, 4]
+    //['Terminal de Ómnibus', 'Av. Mac Lean y Soberanía Nac. Loc 4, Sector A.', 0, 0, 5],
+    ['Quiniela Buena Suerte', 'Av San Martín 2099', -27.4719418, -59.0011489, 6],
+    ['Quiniela Dellacasa', 'Hipólito Yrigoyen 153', -27.4516258, -58.984265, 7],
+    ['Maxikiosco Napoleón', 'Frondizi 299', -27.4543356, -58.9882299, 8],
+    ['Maxikiosco Luki', 'Av 9 de Julio 1050', -27.459342, -58.977215, 9],
+    ['Heladería del Pilar', 'Av Castelli 2507', -27.4774018, -58.971287, 10],
+    //['Multirubro Coco', 'Mz 120, Dpto 3 y 4. B° Golf 1000 Viv.', 0, 0, 11],
+    //['Telecabina "B"', 'Mz 76 Pc 1 Casa 707. B° Santa Inés', 0, 0, 12],
+    ['Teleciber Enter', 'Av Israel 2600', -27.4367905, -59.0135498, 13],
+    //['La Manzanita', 'Padre Cerqueira 1440', 0, 0, 14]
+    ['Caja Municipal', 'Av Italia 102', -27.4540748, -58.981437, 15],
+    ['Caja Municipal', 'Av San Martín 1040', -27.4633534, -58.9912765, 16],
+    ['Kiosco El Profesional', 'Salta 294', -27.4512601, -58.9916807, 17],
+    ['Mi Genio Amor', 'Av Hernandarias 535', -27.449141, -58.9984832, 18],
+    ['Librería Seño Norma', 'Av Hernandarias 2045', -27.4609324, -59.0116237, 19],
+    //['Viviana E. Toros', 'Mz 2 Pc 1 Calle A. del Carril. B° Guiraldes', 0, 0, 20],
+    ['Miriam E. Romero', 'Av Sabín 655', -27.4253902, -58.9777985, 21],
+    ['Maxi Martín', 'Av Alberdi 1402', -27.4624885, -58.9989774, 22]
+  ];
 
   var mapOptions = {
     center: new google.maps.LatLng(-27.4704241, -58.9754468),
     zoom: 13,
-    //disableDefaultUI: true,
+    disableDefaultUI: true,
     mapTypeId: google.maps.MapTypeId.ROADMAP
   };
 
@@ -62,11 +82,11 @@ function initialize() {
         'icon': icon
       });
 
-      /*var infowindow = new google.maps.InfoWindow({
+      var infowindow = new google.maps.InfoWindow({
         map: map,
         position: pos,
-        content: 'Ubicación encontrado usando HTML5.'
-      });*/
+        content: 'Usted está aquí!'
+      });
 
       map.setCenter(pos);
     }, function() {
